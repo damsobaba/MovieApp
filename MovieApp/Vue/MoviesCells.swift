f77e4d264ac071f0c686e235f8da7aa1b89ccd90
adam <ad.mabrouki@gmail.com>
2020-04-30 00:06:56 +0200

    creation des dossier

---
+++ MovieApp/Vue/MoviesCells.swift
@@ -0,0 +1,26 @@
+//
+//  MoviesCells.swift
+//  MovieApp
+//
+//  Created by Adam Mabrouki on 29/04/2020.
+//  Copyright © 2020 Adam Mabrouki. All rights reserved.
+//
+
+import UIKit
+
+class MoviesCells: UITableViewCell {
+
+
+
+    @IBOutlet weak var imgev: UIImageView!
+    
+    
+    @IBOutlet weak var nameLbl: UILabel!
+    
+    
+    @IBOutlet weak var descLbl: UILabel!
+    
+    
+   
+    
+}
